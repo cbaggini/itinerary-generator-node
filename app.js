4622dd5964784cc42558fde60ea23aefe0333076
Cecilia <earwen1986@gmail.com>
2021-03-03 16:31:56 +0000

    Added CORS headers

--- app.js
+++ app.js
@@ -5,11 +5,20 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
-// TODO: setup CORS for localhost or Netlify frontend
-app.use(cors());
-app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 8080;
+const ORIGIN = process.env.PORT
+  ? "https://itinerary-generator.netlify.app"
+  : "*";
+
+const corsOptions = {
+  origin: ORIGIN,
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+// TODO: setup CORS for localhost or Netlify frontend
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome to the itinerary generator!");
