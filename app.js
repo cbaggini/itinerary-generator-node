f7cf63a18623f669093f0e078abb595355148d15
Cecilia <earwen1986@gmail.com>
2021-05-16 15:12:58 +0100

    Initial oauth implementation

--- app.js
+++ app.js
@@ -2,14 +2,50 @@ const itinerary = require("./api/itinerary");
 
 const express = require("express");
 const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const TwitterStrategy = require("passport-twitter").Strategy;
+const GitHubStrategy = require("passport-github").Strategy;
+const mongoose = require("mongoose");
+
+const User = require("./User");
 
 let app = express();
 app.disable("x-powered-by");
+app.set("trust proxy", 1);
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      sameSite: "none",
+      secure: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7, // One Week
+    },
+  })
+);
 
 const PORT = process.env.PORT || 8080;
 const ORIGIN = process.env.PORT
   ? "https://itinerary-generator.netlify.app"
   : "*";
+const BASE_URL = process.env.PORT
+  ? "https://itinerary-generator.netlify.app/"
+  : "http://localhost:8080/";
+
+mongoose.connect(
+  `${process.env.DB}`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("Connected to mongoose successfully");
+  }
+);
 
 const corsOptions = {
   origin: ORIGIN,
@@ -20,6 +56,101 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false, limit: "20mb" }));
 
+passport.serializeUser((user, done) => {
+  return done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, doc) => {
+    // Whatever we return goes to the client and binds to the req.user property
+    return done(null, doc);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: `${process.env.GOOGLE_CLIENT_ID}`,
+      clientSecret: `${process.env.GOOGLE_CLIENT_SECRET}`,
+      callbackURL: "/auth/google/callback",
+    },
+    function (_, __, profile, cb) {
+      User.findOne({ googleId: profile.id }, async (err, doc) => {
+        if (err) {
+          return cb(err, null);
+        }
+
+        if (!doc) {
+          const newUser = new User({
+            googleId: profile.id,
+            username: profile.name.givenName,
+          });
+
+          await newUser.save();
+          cb(null, newUser);
+        }
+        cb(null, doc);
+      });
+    }
+  )
+);
+
+passport.use(
+  new TwitterStrategy(
+    {
+      consumerKey: `${process.env.TWITTER_CLIENT_ID}`,
+      consumerSecret: `${process.env.TWITTER_CLIENT_SECRET}`,
+      callbackURL: "/auth/twitter/callback",
+    },
+    function (_, __, profile, cb) {
+      User.findOne({ twitterId: profile.id }, async (err, doc) => {
+        if (err) {
+          return cb(err, null);
+        }
+
+        if (!doc) {
+          const newUser = new User({
+            twitterId: profile.id,
+            username: profile.username,
+          });
+
+          await newUser.save();
+          cb(null, newUser);
+        }
+        cb(null, doc);
+      });
+    }
+  )
+);
+
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: `${process.env.GITHUB_CLIENT_ID}`,
+      clientSecret: `${process.env.GITHUB_CLIENT_SECRET}`,
+      callbackURL: "/auth/github/callback",
+    },
+    function (_, __, profile, cb) {
+      User.findOne({ githubId: profile.id }, async (err, doc) => {
+        if (err) {
+          return cb(err, null);
+        }
+
+        if (!doc) {
+          const newUser = new User({
+            githubId: profile.id,
+            username: profile.username,
+          });
+
+          await newUser.save();
+          cb(null, newUser);
+        }
+        cb(null, doc);
+      });
+    }
+  )
+);
+
 app.get("/", (req, res) => {
   res.send("Welcome to the itinerary generator!");
 });
@@ -29,6 +160,59 @@ app.get("/geocode", itinerary.geocode);
 app.get("/poi", itinerary.poi);
 
 app.post("/itinerary", itinerary.itinerary);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: BASE_URL,
+    session: true,
+  }),
+  function (req, res) {
+    res.redirect(BASE_URL + "profile");
+  }
+);
+
+app.get("/auth/twitter", passport.authenticate("twitter"));
+
+app.get(
+  "/auth/twitter/callback",
+  passport.authenticate("twitter", {
+    failureRedirect: BASE_URL,
+    session: true,
+  }),
+  function (req, res) {
+    res.redirect(BASE_URL + "profile");
+  }
+);
+
+app.get("/auth/github", passport.authenticate("github"));
+
+app.get(
+  "/auth/github/callback",
+  passport.authenticate("github", {
+    failureRedirect: BASE_URL,
+    session: true,
+  }),
+  function (req, res) {
+    res.redirect(BASE_URL + "profile");
+  }
+);
+
+app.get("/getuser", (req, res) => {
+  res.send(req.user);
+});
+
+app.get("/auth/logout", (req, res) => {
+  if (req.user) {
+    req.logout();
+    res.send("done");
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
